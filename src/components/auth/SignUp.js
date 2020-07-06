246953ee55eef779ceecb93a88052f302fcbe00f
Atul Kumar <atul.kumar3@go-mmt.com>
2020-07-06 22:49:07 +0530

    Added login and sign up

--- src/components/auth/SignUp.js
+++ src/components/auth/SignUp.js
@@ -1,9 +1,28 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "../../css/SignUp.css";
 
 function SignUp() {
   return (
-    <div className="">
-      <h3>Sign up js</h3>
+    <div className="sup-top-container">
+      <div className="sup-wrapper">
+        <h4>Sign Up</h4>
+        <form action="" className="form-wrapper">
+          <input type="text" placeholder="email id..." />
+          <input type="password" name="" id="" placeholder="password..." />
+          <input
+            type="password"
+            name=""
+            id=""
+            placeholder="confirm password..."
+          />
+          <button type="submit">Sign Up</button>
+        </form>
+        <p className="al-mem">Already a member?</p>
+        <p className="su-login">
+          <Link to="/login"> Log In</Link>
+        </p>
+      </div>
     </div>
   );
 }
